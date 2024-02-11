91813ffa3ea5c01a97d23ec8e6687bc746314ad7
nileshpatil1992 <nileshpatil1992@gmail.com>
2024-02-11 11:40:53 +0530

    Add files via upload

---
+++ airflow_spark.py
@@ -0,0 +1,16 @@
+import airflow
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+#from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+
+dag_conf = DAG(
+    dag_id='airflow_spark',schedule_interval= '* * * * *' , start_date=airflow.utils.dates.days_ago(2)
+
+
+)
+
+spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='spark_local',dag=dag_conf)
+
+if  __name__=='__main__':
+    dag_conf.cli()
