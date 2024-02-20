6002b8b3d911df9d24ca26618e851f17718f564f
nileshpatil1992 <nileshpatil1992@gmail.com>
2024-02-20 15:05:31 +0530

    Update airflow_spark.py

--- airflow_spark.py
+++ airflow_spark.py
@@ -5,7 +5,7 @@ from airflow.utils.dates import days_ago
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
 
 dag_conf = DAG(
-    dag_id='airflow_spark',schedule_interval= '* * * * *' , start_date=airflow.utils.dates.days_ago(2)
+    dag_id='airflow_spark',schedule_interval= 'None' , start_date=airflow.utils.dates.days_ago(2)
 
 
 )
