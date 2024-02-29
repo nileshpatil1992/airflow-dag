61ecfa05761a5228d01a8a9b8e0242a3e39cc5b2
nileshpatil1992 <nileshpatil1992@gmail.com>
2024-02-29 11:44:45 +0530

    Update airflow_spark.py

--- airflow_spark.py
+++ airflow_spark.py
@@ -5,13 +5,11 @@ from airflow.utils.dates import days_ago
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
 
 
-try:
-  dag_conf = DAG(dag_id='airflow_spark')
-  spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='spark_default',dag=dag_conf,spark_binary='spark-submit',deploy_mode='client')
-  spark_submit_local
-except Exception as error:
-  print("An exception occurred")
-  print("An exception occurred:", error)
+
+dag_conf = DAG(dag_id='airflow_spark')
+spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='spark_default',dag=dag_conf,spark_binary='spark-submit',deploy_mode='client')
+spark_submit_local
+
 
 if  __name__=='__main__':
     dag_conf.cli()
