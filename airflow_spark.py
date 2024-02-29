3a037cb38fea8710ee55f3f23dfecba360cf8aad
nileshpatil1992 <nileshpatil1992@gmail.com>
2024-02-29 10:44:25 +0530

    Update airflow_spark.py

--- airflow_spark.py
+++ airflow_spark.py
@@ -4,13 +4,27 @@ from airflow.utils.dates import days_ago
 #from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
 
-dag_conf = DAG(
-    dag_id='airflow_spark'
+import time
+print("sleeping")
+time.sleep(300)
+print("wakeup")
 
-
-)
-
-spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='spark_local',dag=dag_conf)
+try:
+  dag_conf = DAG(dag_id='airflow_spark')
+  spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='spark_local',dag=dag_conf)
+except Exception as error:
+  print("An exception occurred")
+  print("An exception occurred:", error)
+  import time
+  print("sleeping")
+  time.sleep(300)
+  print("wakeup")
 
 if  __name__=='__main__':
     dag_conf.cli()
+
+
+import time
+print("sleeping")
+time.sleep(300)
+print("wakeup")
