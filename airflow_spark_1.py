2a9268823d684609a23ae72f79bb328c59358c5d
nileshpatil1992 <nileshpatil1992@gmail.com>
2024-03-01 10:40:12 +0530

    Update airflow_spark_1.py

--- airflow_spark_1.py
+++ airflow_spark_1.py
@@ -7,7 +7,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 
 
 dag_conf = DAG(dag_id='airflow_spark_1')
-spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',kubernetes_conn_id='myk8s',namespace="spark-jobs",api_group="sparkoperator.k8s.io",
+spark_submit_local=SparkSubmitOperator(task_id='sparksubmitjobs',application='sparksubmitcode.py',conn_id='myk8s',namespace="spark-jobs",api_group="sparkoperator.k8s.io",
        api_version="v1beta2",do_xcom_push=True,dag=dag_conf)
 spark_submit_local
 
